7e874232e73ed62eeb304c3b2d96f82466c249d9
i66235 <i66235@sina.com>
2024-08-21 08:42:58 +0800

    1

--- GetShowData.js
+++ GetShowData.js
@@ -1,14 +1,14 @@
-const axios = require('axios');
-const cheerio = require('cheerio');
-const fs = require('fs').promises; // 使用 fs.promises 处理异步文件操作
-const http = require('http');
+//直播房间号
+const rids = ['63136','71415'] 
 
-async function WriteShowData(showroomdata) {
+const fs = require('fs').promises; 
+//将内容写入文件
+async function WriteShowData(showroomdata,path) {
     try {
         // 读取现有数据
         let existingData = [];
         try {
-            const data = await fs.readFile('RoomData.json', 'utf8');
+            const data = await fs.readFile(path, 'utf8');
             existingData = JSON.parse(data);
         } catch (err) {
             // 如果文件不存在或读取失败，返回空数组
@@ -28,22 +28,20 @@ async function WriteShowData(showroomdata) {
 
         // 写入更新后的数据
         const w = JSON.stringify(existingData, null, 2);
-        await fs.writeFile('RoomData.json', w, 'utf8');
-        console.log('File has been updated!');
+        await fs.writeFile(path, w, 'utf8');
+        //console.log('File has been updated!');
     } catch (err) {
         console.error('Error writing file:', err);
     }
 }
 
-
-
-
-async function ReadShowData() {
+//读取文件内容
+async function ReadShowData(path) {
     try {
         // 读取文件
-        const data = await fs.readFile('RoomData.json', 'utf8');
+        const data = await fs.readFile(path, 'utf8');
         const jsonData = JSON.parse(data);
-        console.log('JSON data:', jsonData);
+        //console.log('JSON data:', jsonData);
         return jsonData;
     } catch (err) {
         console.error('Error reading or parsing file:', err);
@@ -51,14 +49,12 @@ async function ReadShowData() {
     }
 }
 
+//获取房间信息
 async function GetRoomData(rid) {
     for (let i = 0; i < rid.length; i++) {
         try {
-            const response = await axios.get('https://m.douyu.com/' + rid[i]);
-            const $ = cheerio.load(response.data);
-            const content = $('#vike_pageContext').html();
-            const parsedObject = JSON.parse(content);
-
+            const response = await fetchAndParseHtml('https://m.douyu.com/' + rid[i]);
+            const parsedObject = JSON.parse(response); 
             const data = {
                 rid: parsedObject.pageProps.room.roomInfo.roomInfo.rid,
                 data: {
@@ -72,69 +68,125 @@ async function GetRoomData(rid) {
                     roomName: parsedObject.pageProps.room.roomInfo.roomInfo.roomName,
                 }
             };
-
-            console.log(parsedObject.pageProps.room.roomInfo.roomInfo);
-            await WriteShowData(data);
-            const s = await ReadShowData();
-            console.log("Updated Data:", s);
-            await sendMail("1585543457@qq.com",'55','123')
             
+            const logs ={
+                rid: parsedObject.pageProps.room.roomInfo.roomInfo.rid,
+                sendtime:0
+            }
+            
+            const re = await ReadShowData('MailLog.json');
+            const inde = re.findIndex(item => item.rid === data.rid);
+            if (inde === -1) {
+                await WriteShowData(logs,'MailLog.json');
+            }
+            console.log('===============================');
+            console.log('主播:', data.data.nickname);
+            console.log('房间标题：', data.data.roomName);
+            console.log('直播状态：', data.data.isLive);
+            
+
+            //判断标题是否有更新
+            const s = await ReadShowData('RoomData.json');
+            const index = s.findIndex(item => item.rid === data.rid);
+            if (index !== -1) {
+                if(s[index].data.roomName !== data.data.roomName){
+                    await sendMail("1585543457@qq.com", data.data.nickname + `更换标题`, data.data.roomName)
+                }
+            } 
+
+            //判断是否直播
+            if(isShowLive(data.data.showTime)){
+                if (data.data.isLive === 1) {
+                    await sendShow(data.data.nickname,'开始直播',data.data.roomName,data.rid)
+                }else{
+                    await sendShow(data.data.nickname,'结束直播',data.data.roomName,data.rid)  
+                }     
+            }
+            
+            //更新房间数据
+            await WriteShowData(data,'RoomData.json');
+            console.log('===============================');
+
         } catch (error) {
             console.error('There was a problem with the request or processing:', error);
         }
     }
 }
 
+//发送邮箱提醒并记录发送时间戳至MailLog.json
+async function sendShow(nickname,params,roomName,rid) {
+    const st = Math.floor(Date.now() / 1000);
+    const w = await ReadShowData('MailLog.json');
+    const ind = w.findIndex(item => item.rid === rid);
+
+    if (ind !== -1) {
+        console.log('消息间隔：',st - w[ind].sendtime);
+        //消息发送间隔 300秒
+        if(st - w[ind].sendtime > 300 || w[ind].sendtime === 0){
+            await sendMail("1585543457@qq.com",nickname + params, roomName)
+            const updatedData = w.map(item => 
+                item.rid === rid ? { ...item, sendtime: st } : item
+            );
+
+            const kw = JSON.stringify(updatedData, null, 2);
+            await fs.writeFile('MailLog.json', kw, 'utf8');
+        }
+    } 
+}
+
+//封装的fetch
+async function fetchAndParseHtml(url) {
+    try {
+      // 发起网络请求
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`网络响应不成功: ${response.status} ${response.statusText}`);
+      }
+      // 获取 HTML 内容
+      const html = await response.text();
+      // 使用正则表达式提取内容
+      const regex = /<script id="vike_pageContext" type="application\/json">([\s\S]*?)<\/script>/i;
+      const match = html.match(regex);
+      const content = match ? match[1] : null;
+      return content
+    } catch (error) {
+      console.error('请求或解析错误:', error);
+    }
+}
+
+
+//判断开播时间戳是否小于30
 function isShowLive(t) {
     const s = Math.floor(Date.now() / 1000);
-    return s > t;
+    return s - t < 30
 }
 
 /*
- * 
+ * 通过GET API接口 发送邮箱
  * @param {邮箱地址} mailTo 
  * @param {邮箱标题} subject 
  * @param {邮箱内容} content 
  * @returns 
  */
 async function sendMail(mailTo, subject, content) {
-
-    const data = JSON.stringify({
-        mail_from: 'email@pliv.cc',
-        password: 'Yuange123',
-        mail_to: mailTo,
-        subject: subject,
-        content: content,
-        subtype: null
-    });
-
-    const options = {
-        hostname: '142.171.168.137',
-        port: 41908,
-        path: '/mail_sys/send_mail_http.json',
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Content-Length': Buffer.byteLength(data),
-        },
-      };
-
-      const req = http.request(options, (res) => {
-        let responseBody = '';
-
-        res.on('data', (chunk) => {
-            responseBody += chunk;
-          });
-
-        res.on('end', () => {
-        console.log('响应内容:', responseBody);
+    const url = `http://142.171.168.137:41908/mail_sys/send_mail_http.json?mail_from=email@pliv.cc&password=Yuange123&mail_to=${mailTo}&subject=${subject}&content=${content}&subtype=`;
+    fetch(url)
+        .then(response => {
+            if (!response.ok) {
+            // 如果响应状态码不是 2xx，抛出错误
+            throw new Error(`网络响应不成功: ${response.status} ${response.statusText}`);
+            }
+            return response.json(); // 解析 JSON 响应
+        })
+        .then(data => {
+            console.log('邮箱状态:', data); // 处理获取的数据
+        })
+        .catch(error => {
+            console.error('邮箱状态:', error); // 处理错误
         });
-        
-      })
-      // 发送请求数据
-      req.write(data);
-      req.end();
-    
+
 }
 
-GetRoomData(['63136', '71415']);
+
+//房间数组
+GetRoomData(rids);
